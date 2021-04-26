509a1542485229000c86812af12034b1cabf4451
louay <louay.jrad@digitarm.com>
2021-04-26 14:52:56 +0100

    add modal

--- js/main.js
+++ js/main.js
@@ -1,23 +1,73 @@
-var open = false;
+let openMenu = false;
+let openModal = false;
 function toggleList() {
-  !this.open
+  !this.openMenu
     ? (document
         .getElementById("listItem1")
         .classList.add("opacity-100", "translate-y-0"),
-      (this.open = !this.open))
+      (this.openMenu = !this.openMenu))
     : (document
         .getElementById("listItem1")
         .classList.remove("opacity-100", "translate-y-0"),
       document
         .getElementById("listItem1")
         .classList.add("opacity-0", "translate-y-1"),
-      (this.open = !this.open));
- 
+      (this.openMenu = !this.openMenu));
 }
 
+function toggleModal() {
+  if (!this.openMenu) {
+    document.getElementById("container").classList.remove("hidden");
+    document.getElementById("modalBg").classList.remove("opacity-0");
+    document.getElementById("modalBg").classList.add("opacity-100");
+    document
+      .getElementById("modal")
+      .classList.add("opacity-100", "translate-y-0", "sm:scale-100");
+    this.openMenu = !this.openMenu;
+  } else {
+    document
+      .getElementById("modal")
+      .classList.remove("opacity-100", "translate-y-0", "sm:scale-100");
+      document
+      .getElementById("modal")
+      .classList.add(
+        "opacity-0",
+        "translate-y-4",
+        "sm:translate-y-0",
+        "sm:scale-95"
+      );
+    document.getElementById("modalBg").classList.remove("opacity-100");
+    document.getElementById("modalBg").classList.add("opacity-0");
+    document.getElementById("container").classList.add("hidden");
+  
+    this.openMenu = !this.openMenu;
+  }
+  // !this.openMenu
+  //   ? (document.getElementById("container").classList.remove("hidden"),
+  //     document
+  //       .getElementById("modal")
+  //       .classList.add("opacity-100", "translate-y-0", "sm:scale-100"),
+  //     (this.openMenu = !this.openMenu))
+  //   : (document
+  //       .getElementById("modal")
+  //       .classList.remove("opacity-100", "translate-y-0", "sm:scale-100"),
+  //     document.getElementById("container").classList.add("hidden"),
+  //     document
+  //       .getElementById("modal")
+  //       .classList.add(
+  //         "opacity-0",
+  //         "translate-y-4",
+  //         "sm:translate-y-0",
+  //         "sm:scale-95"
+  //       ),
+  //     (this.openMenu = !this.openMenu));
+}
 // Close the dropdown menu if the user clicks outside of it
 window.onclick = function (event) {
-  if (!event.target.matches("listItem1") || !event.target.matches("listItem2")) {
+  if (
+    !event.target.matches("listItem1") ||
+    !event.target.matches("listItem2")
+  ) {
     var dropdowns = document.getElementsByClassName("dropdown-content");
     var i;
     for (i = 0; i < dropdowns.length; i++) {
@@ -30,4 +80,4 @@ window.onclick = function (event) {
 };
 focusMethod = function getFocus() {
   document.getElementById("add-Email").focus();
-}
+};
