458e5dd9181827bc9ed74d6b6a4cb14de9701f52
louay <louay.jrad@digitarm.com>
2021-05-11 16:41:10 +0100

    add oher pages

--- css/tailwind.config.js
+++ css/tailwind.config.js
@@ -8,6 +8,18 @@ module.exports = {
       "Over-SemiBold": ["Overpass-SemiBold"],
     },
     fontSize: {
+      'xs': '.75rem',
+      'sm': '.875rem',
+      'tiny': '.875rem',
+       'base': '1rem',
+       'lg': '1.125rem',
+       'xl': '1.25rem',
+       '2xl': '1.5rem',
+      '3xl': '1.875rem',
+      '4xl': '2.25rem',
+       '5xl': '3rem',
+       '6xl': '4rem',
+      '7xl': '5rem',
       "first-header": [
         "42px",
         {
@@ -20,7 +32,6 @@ module.exports = {
           lineHeight: "36px",
         },
       ],
-      // Or with a default line-height as well
       "second-header": [
         "32px",
         {
